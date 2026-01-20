2f265e888a5ed5f374782e82d114d99e75132170
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2026-01-20 16:09:46 -0500

    First ever push

---
+++ Assets/Scripts/ScanAT.cs
@@ -0,0 +1,54 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class ScanAT : ActionTask {
+		public Color scanColour;
+		public int numberOfScanCirclePoints;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			
+		}
+
+		private void DrawCircle(Vector3 center, float radius, Color colour, int numberOfPoints)
+		{
+			Vector3 startPoint, endPoint;
+			int anglePerPoint = 360 / numberOfPoints;
+			for (int i = 1; i <= numberOfPoints; i++)
+			{
+				startPoint = new Vector3(Mathf.Cos(Mathf.Deg2Rad * anglePerPoint * (i-1)), 0, Mathf.Sin(Mathf.Deg2Rad * anglePerPoint * (i-1)));
+				startPoint = center + startPoint * radius;
+				endPoint = new Vector3(Mathf.Cos(Mathf.Deg2Rad * anglePerPoint * i), 0, Mathf.Sin(Mathf.Deg2Rad * anglePerPoint * i));
+				endPoint = center + endPoint * radius;
+				Debug.DrawLine(startPoint, endPoint, colour);
+			}
+
+			
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
